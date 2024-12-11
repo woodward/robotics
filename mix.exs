2945d2bb10a2972abfeff30415ce4b7052b64db4
Greg Woodward <woodward@users.noreply.github.com>
2024-12-10 19:06:06 -0800

    Add license and other hex publishing boilerplate

--- mix.exs
+++ mix.exs
@@ -1,13 +1,17 @@
 defmodule Robotics.MixProject do
   use Mix.Project
 
+  @source_url "https://github.com/woodward/robotics"
+
   def project do
     [
       app: :robotics,
       version: "0.1.0",
       elixir: "~> 1.17",
       start_permanent: Mix.env() == :prod,
-      deps: deps()
+      deps: deps(),
+      description: "Robotics in Elixir",
+      package: package()
     ]
   end
 
@@ -19,7 +23,22 @@ defmodule Robotics.MixProject do
 
   defp deps do
     [
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.35", only: :dev, runtime: false}
+    ]
+  end
+
+  defp package do
+    [
+      maintainers: ["Greg Woodward"],
+      licenses: ["MIT"],
+      links: %{
+        "GitHub" => @source_url
+      },
+      files: [
+        "lib",
+        "mix.exs",
+        "LICENSE.md"
+      ]
     ]
   end
 end
